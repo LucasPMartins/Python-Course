00857f20763326a1f41824f7681e419842870420
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-07-23 14:55:48 -0300

    Ambientes virtuais em Python (venv)

---
+++ Python/Curso-Python-3/Intermediario/aula112.py
@@ -0,0 +1,23 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é o módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+
+# python -m venv venv
+# python -modulo venv nome
+
+# python global = C:\Users\LucasMartins\AppData\Local\Programs\Python\Python312\python.exe
+# pip install nome==versao
+# ou pip install nome (versão mais rescente)
+# pip unistall nome
+
+# pip freeze (lista as dependencias)
+# pip freeze > requirements.txt (cria a lista as dependencias)
+# pip install -r requirements.txt
+
