964f285a2249658839eef4eb3e89be345751d67c
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-07-23 14:35:04 -0300

    exercicio de imc

---
+++ Python/Curso-Python-3/Basico/aula12.py
@@ -0,0 +1,8 @@
+nome = "Lucas"
+peso = 67.0
+altura = 1.75
+imc = peso/(altura**2)
+
+print(nome + " tem " + str(altura) + " de altura,",end="")
+print("pesa" , peso, "quilos e seu IMC é ", end="")
+print(imc)
