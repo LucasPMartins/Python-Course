65ebb085225eff5881cc19d56edbe62b583e9560
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-07-31 16:34:16 -0300

    dir e help + DocStrings

---
+++ Python/Curso-Python-3/POO/aula153/varias_linhas.py
@@ -0,0 +1,10 @@
+'''O que seu módulo faz
+O recomentado é usar aspas duplas
+asdoadapsdaçsdasda
+daohgodhgaksdgkjasdgas
+sdagajhdgaksjhgkasdg
+sdagadjhglkasglasgasg
+sadasdsafsafsdfsadf
+sadgasdgadga
+gadgadgagasg
+'''
