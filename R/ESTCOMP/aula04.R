235378d628c415422d6b55353c33c94dd3c377a0
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-08-22 22:38:29 -0300

    titanic com ggplot2

--- R/ESTCOMP/aula04.R
+++ R/ESTCOMP/aula04.R
@@ -1,10 +1,15 @@
+# O parâmetro 'header = TRUE' indica que a primeira linha do arquivo contém os nomes das colunas.
+# O parâmetro 'sep = ","' colunas separadas por vírgulas (formato CSV).
 dados <- read.table(file = "titanic.txt", header = TRUE, sep = ",")
 dados <- dados[,-c(1,9:12)]
+# Remover as colunas 1, 9, 10, 11 e 12 do data frame 'titanic'.
+# A notação '-c(1,9:12)' indica que as colunas não devem ser incluídas no novo data frame
 
 dados$Survived <- as.factor(dados$Survived)
 dados$Pclass <- as.factor(dados$Pclass)
 dados$Sex <- as.factor(dados$Sex)
 
+# Exibir a estrutura do data frame 'titanic'.
 str(dados)
 
 summary(dados)
