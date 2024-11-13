2e2934de33044a6b923123c46a2fc5ed884baa32
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-11-13 13:40:50 -0300

    Trabalhando com classes e herança no PySide6

--- Curso-Python-3/PySide6/aula05.py
+++ Curso-Python-3/PySide6/aula05.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyWindow()
     window.show()
-    app.exec()  # O loop da aplicação
+    app.exec()  # O loop da aplicação 
